54ec8f55bfce04e3fac9fd0bb5d63a82a14fe7b8
Serge Ngang <Serge.Ngang@student.lut.fi>
2026-09-04 19:43:08 +0200

    one more error solved

--- script.js
+++ script.js
@@ -3,16 +3,15 @@ const heading = document.querySelector("h1");
 const addDataButton = document.getElementById("add-data");
 const myList = document.getElementById("my-list");
 const myTextarea = document.getElementById("my-textarea");
+
 myButton.addEventListener("click", () => {
     console.log("hello world");
     heading.textContent = "Moi maailma";
 });
+
 addDataButton.addEventListener("click", () => {
-    const textValue = myTextarea.value.trim();
-    if (textValue !== "") {
-        const newItem = document.createElement("li");
-        newItem.textContent = textValue;
-        myList.appendChild(newItem);
-        myTextarea.value = "";
-    }
+    const newItem = document.createElement("li");
+    newItem.textContent = myTextarea.value;
+    myList.appendChild(newItem);
+    myTextarea.value = "";
 });
